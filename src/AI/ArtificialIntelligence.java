6021ebc4953d8cb64ea587d9b5055d5539fb8634
Kamil <Kamil@kkb-hp.wireless.unimaas.local>
2016-05-13 13:40:53 +0200

    Something something

---
+++ src/AI/ArtificialIntelligence.java
@@ -0,0 +1,12 @@
+package AI;
+
+public class ArtificialIntelligence {
+
+	private Simulation sim;
+	private AIAlgorithm alg;
+	
+	public ArtificialIntelligence(Simulation s, AIAlgorithm a) {
+		sim = s;
+		alg = a;
+	}
+}
